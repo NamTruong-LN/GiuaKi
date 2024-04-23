adb159628231325a2a3043b93e8023f30ca4451b
NamTruong-LN <gitblue8469@gmail.com>
2024-04-23 17:00:07 +0700

    Giua ki

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+//import Demo from './Compoments/Demo';
+//import LoginComponent from './Compoments/demo2803';
+import Test1 from './Compoments/Demo1104';
+function App() {
+  return (
+    <div>
+      <Test1/>
+    </div>
+  )
+}
+
+export default App;
